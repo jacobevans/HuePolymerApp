cd351fd68a18c14e48aabb033342771df5708b4f
Jacob Evans <jacobevans@livenation.com>
2015-09-24 19:39:21 -0700

    adding/removing lights from groups works.  a little janky of an implementation though...needs cleanup

--- main.js
+++ main.js
@@ -2,3 +2,9 @@
 // toastGroupTemplate.showToast = function() {
 //   document.querySelector('#toast').show();
 // }
+// ;//.text("You can't remove the last light from a group.");
+window.displayAlert = function(text){
+  var toast = document.querySelector("paper-toast#toast");
+  toast.text = text;
+  toast.show();
+}
